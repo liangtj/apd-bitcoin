25b918a32a402c58fdda40e340f91cd22cae9b06
xingxiaoshi <xingxiaoshi@tencent.com>
2018-07-28 17:57:36 +0800

    init AutoBitcoin project

---
+++ AutoBitcoin/__init__.py
@@ -0,0 +1,4 @@
+import pymysql
+
+# mysql 数据库初始化
+pymysql.install_as_MySQLdb()
---
+++ BitcoinDealer/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BitcoindealerConfig(AppConfig):
+    name = 'BitcoinDealer'
