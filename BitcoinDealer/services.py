98ca950adb749194a4f836336334102d444dfd1b
skyliang <skyliang@tencent.com>
2018-08-02 14:28:21 +0800

    后台逻辑层封装完毕

--- BitcoinDealer/services.py
+++ BitcoinDealer/services.py
@@ -84,7 +84,7 @@ def get_detail(symbol):
     return http_get_request(url, params)
 
 # 获取  支持的交易对
-def get_symbols(long_polling=None, request):
+def get_symbols(request, long_polling=None):
     return HuobiServices.get_symbols(
         long_polling, 
         getAccessKey(request),
@@ -115,7 +115,7 @@ def get_balance(request):
 # 下单
 
 # 创建并执行订单
-def send_order(amount, source, symbol, _type, request, price=0):
+def send_order(request, amount, source, symbol, _type, price=0):
     """
     :param amount: 
     :param source: 如果使用借贷资产交易，请在下单接口,请求参数source中填写'margin-api'
@@ -133,7 +133,7 @@ def send_order(amount, source, symbol, _type, request, price=0):
     )
 
 # 撤销订单
-def cancel_order(order_id, request):
+def cancel_order(request, order_id):
     return HuobiServices.cancel_order(
         order_id,
         getAccessKey(request),
@@ -142,7 +142,7 @@ def cancel_order(order_id, request):
 
 
 # 查询某个订单
-def order_info(order_id, request):
+def order_info(request, order_id):
     return HuobiServices.order_info(
         order_id,
         getAccessKey(request),
@@ -151,7 +151,7 @@ def order_info(order_id, request):
 
 
 # 查询某个订单的成交明细
-def order_matchresults(order_id, request):
+def order_matchresults(request, order_id):
     return HuobiServices.order_matchresults(
         order_id,
         getAccessKey(request),
@@ -161,7 +161,8 @@ def order_matchresults(order_id, request):
 
 # 查询当前委托、历史委托
 def orders_list(
-    symbol, states, request, 
+    request, 
+    symbol, states, 
     types=None, start_date=None, 
     end_date=None, _from=None, 
     direct=None, size=None
@@ -189,7 +190,13 @@ def orders_list(
 
 
 # 查询当前成交、历史成交
-def orders_matchresults(symbol, ACCESS_KEY, SECRET_KEY, types=None, start_date=None, end_date=None, _from=None, direct=None, size=None):
+def orders_matchresults(
+    request,
+    symbol, types=None, 
+    start_date=None, end_date=None, 
+    _from=None, direct=None, 
+    size=None
+    ):
     """
     
     :param symbol: 
@@ -201,27 +208,23 @@ def orders_matchresults(symbol, ACCESS_KEY, SECRET_KEY, types=None, start_date=N
     :param size: 
     :return: 
     """
-    params = {'symbol': symbol}
-
-    if types:
-        params[types] = types
-    if start_date:
-        params['start-date'] = start_date
-    if end_date:
-        params['end-date'] = end_date
-    if _from:
-        params['from'] = _from
-    if direct:
-        params['direct'] = direct
-    if size:
-        params['size'] = size
-    url = '/v1/order/matchresults'
-    return api_key_get(params, url, ACCESS_KEY, SECRET_KEY)
+    return HuobiServices.orders_matchresults(
+        symbol,
+        getAccessKey(request),
+        getSecretKey(request),
+        types, start_date,
+        end_date, _from,
+        direct, size
+    )
 
 
 
 # 申请提现虚拟币
-def withdraw(address, amount, currency, ACCESS_KEY, SECRET_KEY, fee=0, addr_tag=""):
+def withdraw(
+    request, address,
+    amount, currency,
+    fee=0, addr_tag=""
+    ):
     """
 
     :param address_id: 
@@ -234,17 +237,15 @@ def withdraw(address, amount, currency, ACCESS_KEY, SECRET_KEY, fee=0, addr_tag=
               "data": 700
             }
     """
-    params = {'address': address,
-              'amount': amount,
-              "currency": currency,
-              "fee": fee,
-              "addr-tag": addr_tag}
-    url = '/v1/dw/withdraw/api/create'
-
-    return api_key_post(params, url, ACCESS_KEY, SECRET_KEY)
+    return HuobiServices.withdraw(
+        address, amount, currency,
+        getAccessKey(request),
+        getSecretKey(request),
+        fee, addr_tag
+    )
 
 # 申请取消提现虚拟币
-def cancel_withdraw(address_id, ACCESS_KEY, SECRET_KEY):
+def cancel_withdraw(request, address_id):
     """
 
     :param address_id: 
@@ -253,10 +254,11 @@ def cancel_withdraw(address_id, ACCESS_KEY, SECRET_KEY):
               "data": 700
             }
     """
-    params = {}
-    url = '/v1/dw/withdraw-virtual/{0}/cancel'.format(address_id)
-
-    return api_key_post(params, url, ACCESS_KEY, SECRET_KEY)
+    return HuobiServices.cancel_withdraw(
+        address_id,
+        getAccessKey(request),
+        getSecretKey(request)
+    )
 
 
 '''
@@ -266,7 +268,7 @@ def cancel_withdraw(address_id, ACCESS_KEY, SECRET_KEY):
 # 创建并执行借贷订单
 
 
-def send_margin_order(amount, source, symbol, _type, ACCESS_KEY, SECRET_KEY, price=0):
+def send_margin_order(request, amount, source, symbol, _type, price=0):
     """
     :param amount: 
     :param source: 'margin-api'
@@ -275,120 +277,107 @@ def send_margin_order(amount, source, symbol, _type, ACCESS_KEY, SECRET_KEY, pri
     :param price: 
     :return: 
     """
-    try:
-        accounts = get_accounts(ACCESS_KEY, SECRET_KEY)
-        acct_id = accounts['data'][0]['id']
-    except BaseException as e:
-        print ('get acct_id error.%s' % e)
-        acct_id = ACCOUNT_ID
-
-    params = {"account-id": acct_id,
-              "amount": amount,
-              "symbol": symbol,
-              "type": _type,
-              "source": 'margin-api'}
-    if price:
-        params["price"] = price
-
-    url = '/v1/order/orders/place'
-    return api_key_post(params, url, ACCESS_KEY, SECRET_KEY)
+    return HuobiServices.send_margin_order(
+        amount, source,
+        symbol, _type,
+        getAccessKey(request),
+        getSecretKey(request),
+        price
+    )
 
 # 现货账户划入至借贷账户
 
 
-def exchange_to_margin(symbol, currency, amount, ACCESS_KEY, SECRET_KEY):
+def exchange_to_margin(symbol, currency, amount, request):
     """
     :param amount: 
     :param currency: 
     :param symbol: 
     :return: 
     """
-    params = {"symbol": symbol,
-              "currency": currency,
-              "amount": amount}
-
-    url = "/v1/dw/transfer-in/margin"
-    return api_key_post(params, url, ACCESS_KEY, SECRET_KEY)
+    return HuobiServices.exchange_to_margin(
+        symbol, currency, amount
+        getAccessKey(request),
+        getSecretKey(request)
+    )
 
 # 借贷账户划出至现货账户
 
 
-def margin_to_exchange(symbol, currency, amount, ACCESS_KEY, SECRET_KEY):
+def margin_to_exchange(request, symbol, currency, amount):
     """
     :param amount: 
     :param currency: 
     :param symbol: 
     :return: 
     """
-    params = {"symbol": symbol,
-              "currency": currency,
-              "amount": amount}
-
-    url = "/v1/dw/transfer-out/margin"
-    return api_key_post(params, url, ACCESS_KEY, SECRET_KEY)
+    return HuobiServices.margin_to_exchange(
+        symbol, currency,
+        amount,
+        getAccessKey(request),
+        getSecretKey(request)
+    )
 
 # 申请借贷
-def get_margin(symbol, currency, amount, ACCESS_KEY, SECRET_KEY):
+def get_margin(request, symbol, currency, amount):
     """
     :param amount: 
     :param currency: 
     :param symbol: 
     :return: 
     """
-    params = {"symbol": symbol,
-              "currency": currency,
-              "amount": amount}
-    url = "/v1/margin/orders"
-    return api_key_post(params, url)
+    return HuobiServices.get_margin(
+        symbol, currency, amount,
+        getAccessKey(request),
+        getSecretKey(request)
+    )
 
 # 归还借贷
-def repay_margin(order_id, amount, ACCESS_KEY, SECRET_KEY):
+def repay_margin(request, order_id, amount):
     """
     :param order_id: 
     :param amount: 
     :return: 
     """
-    params = {"order-id": order_id,
-              "amount": amount}
-    url = "/v1/margin/orders/{0}/repay".format(order_id)
-    return api_key_post(params, url, ACCESS_KEY, SECRET_KEY)
+    return HuobiServices.repay_margin(
+        order_id, amount,
+        getAccessKey(request),
+        getSecretKey(request)
+    )
 
 # 借贷订单
-def loan_orders(symbol, currency, ACCESS_KEY, SECRET_KEY, start_date="", end_date="", start="", direct="", size=""):
+def loan_orders(
+    request,
+    symbol, currency, start_date="", 
+    end_date="", start="", direct="", 
+    size=""):
     """
     :param symbol: 
     :param currency: 
     :param direct: prev 向前，next 向后
     :return: 
     """
-    params = {"symbol": symbol,
-              "currency": currency}
-    if start_date:
-        params["start-date"] = start_date
-    if end_date:
-        params["end-date"] = end_date
-    if start:
-        params["from"] = start
-    if direct and direct in ["prev", "next"]:
-        params["direct"] = direct
-    if size:
-        params["size"] = size
-    url = "/v1/margin/loan-orders"
-    return api_key_get(params, url, ACCESS_KEY, SECRET_KEY)
+    return HuobiServices.loan_orders(
+        symbol, currency,
+        getAccessKey(request),
+        getSecretKey(request),
+        start_date, end_date,
+        start, direct,
+        size
+    )
 
 
 # 借贷账户详情,支持查询单个币种
-def margin_balance(symbol, ACCESS_KEY, SECRET_KEY):
+def margin_balance(request, symbol):
     """
     :param symbol: 
     :return: 
     """
-    params = {}
-    url = "/v1/margin/accounts/balance"
-    if symbol:
-        params['symbol'] = symbol
-    
-    return api_key_get(params, url, ACCESS_KEY, SECRET_KEY)
+    return HuobiServices.margin_balance(
+        symbol,
+        getAccessKey(request),
+        getSecretKey(request)
+    )
 
 
 if __name__ == '__main__':
