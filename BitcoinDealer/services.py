78aebe3598e99ed47f36698bd774893de4454ad6
skyliang <skyliang@tencent.com>
2018-08-06 16:44:39 +0800

    fix bugs of pack

--- BitcoinDealer/services.py
+++ BitcoinDealer/services.py
@@ -1,19 +1,6 @@
-from TradingStrategy import autoTrade
-import .basicServices
+# from TradingStrategy import autoTrade
+from BitcoinDealer import basicServices
 import json
-# """
-# :param buying_decline: 买入跌幅
-# :param selling_gain: 卖出涨幅
-# :param purchase_quantity: 买入金额
-# :param min_num_of_positions: 最少补仓次数
-# :param max_num_of_positions: 最多补仓次数
-# :param stop_loss_ratio: 资产止损比例
-# :param take_profit_ratio: 资产止盈比例
-# :param monitoring_time: 监控时长
-# :return: {
-    # status: "pending" | "finished" | "failed"
-}
-# """
 
 '''
 Market data API
@@ -40,20 +27,20 @@ def send_order(request, amount, source, symbol, _type):
 def orders_submitted_history():
     return basicServices.orders_matchresults(request, "btcusdt")
 
-def auto_trade(
-    request, buying_decline, selling_gain,
-    purchase_quantity, min_num_of_positions,
-    max_num_of_positions, stop_loss_ratio,
-    take_profit_ratio, monitoring_time):
-
-    accountBalanceLists = basicServices.get_balance(request)["data"]["list"]
-    bal = list(filter(lambda x: x["currency"]=="usdt" and x["type"] == 'trade', accountBalanceLists))
-
-    if bal[0] > purchase_quantity:
-        return json.dumps({status: "failed", msg: "purchase_quantity is too large"})
-    return TradingStrategy.autoTrade(
-        request, buying_decline, selling_gain,
-        purchase_quantity, min_num_of_positions,
-        max_num_of_positions, stop_loss_ratio,
-        take_profit_ratio, monitoring_time
-    )
+# def auto_trade(
+#     request, buying_decline, selling_gain,
+#     purchase_quantity, min_num_of_positions,
+#     max_num_of_positions, stop_loss_ratio,
+#     take_profit_ratio, monitoring_time):
+# 
+#     accountBalanceLists = basicServices.get_balance(request)["data"]["list"]
+#     bal = list(filter(lambda x: x["currency"]=="usdt" and x["type"] == 'trade', accountBalanceLists))
+# 
+#     if bal[0] > purchase_quantity:
+#         return json.dumps({status: "failed", msg: "purchase_quantity is too large"})
+#     return TradingStrategy.autoTrade(
+#         request, buying_decline, selling_gain,
+#         purchase_quantity, min_num_of_positions,
+#         max_num_of_positions, stop_loss_ratio,
+#         take_profit_ratio, monitoring_time
+#     )
